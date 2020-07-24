1131a5953e50d4e633f956e06cffe4335c85f119
jbqneto <jbqneto@gmail.com>
2020-07-24 11:34:59 -0300

    Criado estrutura de Revenda. Model e Repository

---
+++ src/main/java/io/jbqneto/desafioindra/repositories/empresa/RevendaRepository.java
@@ -0,0 +1,9 @@
+package io.jbqneto.desafioindra.repositories.empresa;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import io.jbqneto.desafioindra.models.empresa.Revenda;
+
+public interface RevendaRepository extends JpaRepository<Revenda, Long> {
+
+}
