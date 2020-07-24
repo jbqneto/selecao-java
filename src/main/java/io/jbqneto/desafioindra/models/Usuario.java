d216e00f69242d4682ca23e3990486c67aa4f62e
jbqneto <jbqneto@gmail.com>
2020-07-24 01:36:46 -0300

    Adicionado campos do banco e validacoes ao model usuario

--- src/main/java/io/jbqneto/desafioindra/models/Usuario.java
+++ src/main/java/io/jbqneto/desafioindra/models/Usuario.java
@@ -2,11 +2,16 @@ package io.jbqneto.desafioindra.models;
 
 import java.io.Serializable;
 
+import javax.persistence.Column;
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
 import javax.persistence.Table;
+import javax.validation.constraints.NotBlank;
+
+import com.fasterxml.jackson.annotation.JsonProperty;
+import com.fasterxml.jackson.annotation.JsonProperty.Access;
 
 @Entity
 @Table(name = "USUARIO")
@@ -17,12 +22,23 @@ public class Usuario implements Serializable {
 	@GeneratedValue(strategy = GenerationType.AUTO)
 	private long id;
 	
+	@Column(name = "NOME", nullable = false)
+	@NotBlank(message = "Necessárion informar o campo 'nome'.")
 	private String nome;
 	
+	@Column(name = "EMAIL", unique = true, nullable = false)
+	@NotBlank(message = "Necessárion informar o campo 'e-mail'.")
 	private String email;
 	
+	@Column(name = "LOGIN", unique = true, nullable = false)
+	@NotBlank(message = "Necessárion informar o campo 'login'.")
 	private String login;
 	
+	private String sobre;
+	
+	@Column(name = "SENHA", length = 100, nullable = false)
+	@JsonProperty(access = Access.WRITE_ONLY)
+	@NotBlank(message = "Necessárion informar o campo 'senha'.")
 	private String senha;
 
 	public long getId() {
@@ -67,6 +83,14 @@ public class Usuario implements Serializable {
 
 	public static long getSerialversionuid() {
 		return serialVersionUID;
+	}
+
+	public String getSobre() {
+		return sobre;
+	}
+
+	public void setSobre(String sobre) {
+		this.sobre = sobre;
 	}
 	
 }
